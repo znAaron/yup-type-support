7ea8be3d2ac9ac3c24ff4767b4d61e797a45f74b
Zinan Guo <guozinan2000@gmail.com>
2022-07-10 21:20:00 -0400

    ignore build

--- build/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function guessSchema() {
-    // @ts-ignore
-    const placeHolder = "I am a place holder";
-    return placeHolder;
-}
-exports.default = guessSchema;

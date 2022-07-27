fb5e37b80566b8cebcf17507ce118ec5e8247a30
Zinan Guo <zinan.guo@slalom.com>
2022-07-27 09:57:51 -0500

    more test cases

--- src/tests/exampleInterface.test.ts
+++ src/tests/exampleInterface.test.ts
@@ -1,6 +1,6 @@
 import { simpleAccountSchema } from "./exampleInterface";
 
-test('simple schema validation', () => {
+test('simple schema validation pass', () => {
   const mockSimpleAccount_valid = {
     id: 1234,
     name: "Bank",
@@ -9,5 +9,15 @@ test('simple schema validation', () => {
   expect(() => {
     simpleAccountSchema.validateSync(mockSimpleAccount_valid)
   }).not.toThrow()
+});
+
+test('simple schema validation fail', () => {
+  const mockSimpleAccount_valid = {
+    id: 1234,
+    name: "Bank",
+  };
+  expect(() => {
+    simpleAccountSchema.validateSync(mockSimpleAccount_valid)
+  }).toThrow()
 });
 
